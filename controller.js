dcbee677ec61af4ff728bdcddb2874af76a3c0ce
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-01-13 17:55:19 +0900

    Create controller.js

---
+++ controller.js
@@ -0,0 +1,28 @@
+;(function(root){
+ let fn={}
+ fn.q=(s,doc=document)=>{return doc.querySelector(s)}
+ let c={}
+ c.keys={37:'<',39:'>',38:'^',40:'v',70:'A',68:'B',65:'X',83:'Y',82:'R',69:'L'}
+ c.key0=''
+ c.key1=''
+ c.block=0
+ c.flash=()=>{c.key0=c.key1=''}
+ //c.fps=10
+ c.done=(/*fps,*/debughtml)=>{
+  //c.fps=fps||c.fps;
+  document.documentElement.onkeydown=function(ev){
+   if(!c.keys[ev.which])return;
+   if(c.block)return;  
+   if(c.key0) c.key1=c.key0;
+   c.key0=c.keys[ev.which]
+  }
+  ;
+  //setInterval(()=>{ c.key0='' },1000/c.fps)
+  ;
+  if(!fn.q(debughtml))return;
+  setInterval(()=>{fn.q(debughtml).textContent=c.key0+'　'},1000/60)
+ }
+ ;
+ root.controller=c;
+ //controller.done('pre')
+})(this);
